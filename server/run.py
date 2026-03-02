7bb1eef42f736c1a6c3a4a41eaa13dae4feba148
subbu2107 <subbramaniyan2107@gmail.com>
2026-03-03 00:33:05 +0530

    Add files via upload

--- server/run.py
+++ server/run.py
@@ -1,8 +1,16 @@
 from app import create_app, socketio
 import os
 
-flask_app = create_app()
+# 'app' is the name Gunicorn will look for by default
+app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    socketio.run(flask_app, host='127.0.0.1', port=port, debug=True, allow_unsafe_werkzeug=True)
+    # Render provides the PORT environment variable
+    port = int(os.getenv('PORT', 10000))
+    # Listen on 0.0.0.0 in production to accept external connections
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=port,
+        debug=False
+    )
